4ddd5dd83f8415d2ef86af9f856d964e2e953226
Dipanshu <dipanshu231099@gmail.com>
2020-03-31 20:08:20 +0530

    attempt for admin page

--- main_site/routes/users.js
+++ main_site/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var inventory = require('../models/inventory');
+var projects = require('../models/projects');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  inventory.find({}, function(err, products) {
+    console.log(products);
+    res.render('admin', { products: products });
+  });
 });
 
 module.exports = router;
