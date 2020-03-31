af95158971a4176782746fbfa6cf8af23c61894d
Dipanshu <dipanshu231099@gmail.com>
2020-03-31 15:58:25 +0530

    shifted on to expressjs

---
+++ main_site/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//--------club inventory page----------
+router.get('/inventory', function(req, res, next) {
+  res.render('inventory', { title: 'Express' });
+});
+
+
+//-----notifications page---------
+router.get('/notifications', function(req, res, next) {
+  res.render('notifications', { title: 'Express' });
+});
+
+
+//---------projects page --------------
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Express' });
+});
+
+module.exports = router;
