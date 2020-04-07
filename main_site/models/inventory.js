32d164ccadac95bf875088e13bed4da854a272a5
Dipanshu <dipanshu231099@gmail.com>
2020-04-07 13:39:38 +0530

    made website fetch from the database

--- main_site/models/inventory.js
+++ main_site/models/inventory.js
@@ -5,8 +5,10 @@ var Schema = mongoose.Schema;
 var inventorySchema = new Schema(
     {
         name: {type: String, required: true, max: 100},
+        total: {type: Number, required:true, min: 0},
         available: {type: Number, required: true, min: 0},
+        price: {type: Number, min: 0},
     }
 );
 
-module.exports = mongoose.model('inventory',inventorySchema);
+module.exports = mongoose.model('inventory',inventorySchema,'components');
