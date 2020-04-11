326642e47d09c42c016f01e83caae7bbafcc9f73
nidhijainpnp <12njain06@gmail.com>
2020-04-11 17:55:50 +0530

    added backend for projecs and news form

--- main_site/routes/index.js
+++ main_site/routes/index.js
@@ -130,4 +130,129 @@ router.post('/inventory/:id/update', function (req, res, next) {
   });
 });
 
+
+//----------news forms-----------
+router.get('/news/create', function (req, res, next) {
+  res.render('news_form', { action: "Create", results: 0, del: 0 })
+})
+
+router.post('/news/create', function (req, res, next) {
+  var component = new News(
+    {
+      title: req.body.title,
+      content: req.body.content,
+      date_created: req.body.date_created
+     
+    }
+  );
+  component.save(function (err) {
+    if (err) { return next(err); }
+    res.redirect('../admin');
+  })
+})
+
+router.get('/news/:id/delete', function (req, res, next) {
+  var id = req.params.id;
+  News.find({ _id: id }).exec(function (err, results) {
+    console.log(results);
+    res.render('news_form', { action: "Delete", results: results, del: 1 });
+  });
+})
+
+router.post('/news/:id/delete', function (req, res, next) {
+  News.findByIdAndRemove(req.params.id, function deleteComponent(err) {
+    if (err) { return next(err); }
+    // Success - go to author list
+    res.redirect('../../admin')
+  });
+})
+
+router.get('/news/:id/update', function (req, res, next) {
+  var id = req.params.id;
+  News.find({ _id: id }).exec(function (err, results) {
+    console.log(results);
+    res.render('news_form', { action: "Update", results: results, del: 0 });
+  });
+})
+
+router.post('/news/:id/update', function (req, res, next) {
+  var component = new News(
+    {
+      title: req.body.title,
+      content: req.body.content,
+      date_created: req.body.date_created,
+      _id: req.params.id
+    }
+  );
+  News.findByIdAndUpdate(req.params.id, component, {}, function (err, thecomponent) {
+    if (err) { return next(err); }
+    res.redirect('../../admin');
+  });
+});
+
+//----------projects forms-----------
+router.get('/projects/create', function (req, res, next) {
+  res.render('projects_form', { action: "Create", results: 0, del: 0 })
+})
+
+router.post('/projects/create', function (req, res, next) {
+  var component = new Projects(
+    {
+      name: req.body.name,
+      description:req.body.description,
+      mentor: req.body.mentor,
+      team:req.body.team,
+      
+      date_of_creation:req.body.date_of_creation
+     
+    }
+  );
+  component.save(function (err) {
+    if (err) { return next(err); }
+    res.redirect('../admin');
+  })
+})
+
+router.get('/projects/:id/delete', function (req, res, next) {
+  var id = req.params.id;
+  Projects.find({ _id: id }).exec(function (err, results) {
+    console.log(results);
+    res.render('projects_form', { action: "Delete", results: results, del: 1 });
+  });
+})
+
+router.post('/projects/:id/delete', function (req, res, next) {
+  Projects.findByIdAndRemove(req.params.id, function deleteComponent(err) {
+    if (err) { return next(err); }
+    // Success - go to author list
+    res.redirect('../../admin')
+  });
+})
+
+router.get('/projects/:id/update', function (req, res, next) {
+  var id = req.params.id;
+  Projects.find({ _id: id }).exec(function (err, results) {
+    console.log(results);
+    res.render('projects_form', { action: "Update", results: results, del: 0 });
+  });
+})
+
+router.post('/projects/:id/update', function (req, res, next) {
+  var component = new Projects(
+    {
+      name: req.body.name,
+      description:req.body.description,
+      mentor: req.body.mentor,
+      team:req.body.team,
+      
+      date_of_creation:req.body.date_of_creation,
+      _id: req.params.id
+    }
+  );
+  Projects.findByIdAndUpdate(req.params.id, component, {}, function (err, thecomponent) {
+    if (err) { return next(err); }
+    res.redirect('../../admin');
+  });
+});
+
 module.exports = router;
